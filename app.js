346ff3c953aeff56c71e09921f5dfe0c1769d7eb
muzammil-hue <muzammilshafi09@gmail.com>
2025-09-04 16:15:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -16,3 +16,13 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get('/secret', async (req, res) => {
+  try {
+    const secret = await client.getSecret('DB');
+    res.send(secret.value);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});

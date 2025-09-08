286dbd659bbffcde11363b10a0874bde491dcffa
muzammil-hue <muzammilshafi09@gmail.com>
2025-09-08 17:35:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,38 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 
+
+
+const { AppConfigurationClient } = require("@azure/app-configuration");
+const { DefaultAzureCredential } = require("@azure/identity");
+
 const app = express();
 app.use(bodyParser.json());
 
 // Serve static files (HTML, CSS, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+async function loadSecrets() {
+    try {
+        const endpoint = process.env.APPCONFIG_ENDPOINT;
+        const credential = new DefaultAzureCredential();
+        const client = new AppConfigurationClient(endpoint, credential);
+
+        const Username = await client.getConfigurationSetting({ key: "app:login:Username" });
+        const Email = await client.getConfigurationSetting({ key: "app:login:Email" });
+
+        // Store in process.env so routes can use them
+        process.env.LOGIN_USERNAME = Username.value;
+        process.env.LOGIN_PASSWORD = Email.value;
+
+        console.log("Secrets loaded from Key Vault via App Config ✅");
+    } catch (err) {
+        console.error("Error loading secrets:", err.message);
+    }
+}
+
+loadSecrets();
 
 app.use('/api', userRoutes);
 
